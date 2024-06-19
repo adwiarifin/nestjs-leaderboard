adab79964de6c5ad78bdc5a237843885d7322ee1
Adwi Arifin <adwiarifin@gmail.com>
2024-06-19 19:55:00 +0700

    create empty module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { LeaderboardModule } from './leaderboard/leaderboard.module';
+import { ScoreModule } from './score/score.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, LeaderboardModule, ScoreModule],
 })
 export class AppModule {}
